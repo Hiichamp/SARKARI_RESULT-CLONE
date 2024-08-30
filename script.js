ca71c4f2b1e929a8625d30132da0f59715f6ef4d
hiichamp <himanshukumar13333@gmail.com>
2024-08-30 07:57:26 +0530

    git push -u origin

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ gsap.from(".peace",{
     y: 20,
     
     duration: 2,
-    stagger: 0.1,
-    delay: (index, total) => index * 0.5,
+    stagger: 0.2,
+    delay: (index, total) => index * 0.6,
     ease: "elastic.out(1, 0.3)"
 })
 
@@ -14,9 +14,19 @@ gsap.from(".box-mng button",{
     duration: 2,
     stagger: 0.1,
     delay: (index, total) => index * 0.3,
-    ease: "elastic.out(1, 0.3)"
-   
+    ease: "elastic.out(1, 0.3)",
+ 
 })
 
 
- 
+ gsap.from(".footer-container",{
+    opacity: 0,
+    y: -20,
+    delay:1,
+  
+  
+    scrollTrigger:{
+        scroll:"body",
+        trigger:".footer-container"
+    }
+ })
